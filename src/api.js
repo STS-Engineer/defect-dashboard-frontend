5b21630ced772287503303ed0c17f615fee4867d
STS-Engineer <administration.sts@avocarbon.com>
2026-06-17 10:15:40 +0100

    Update api.js port configuration

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: "csl-back.azurewebsites.net",
 });
 
 export const deleteResource = (endpoint, id) => api.delete(`${endpoint}/${id}`);
