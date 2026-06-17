d3730061674c2c19f84cc82d21d56650447a7e02
STS-Engineer <administration.sts@avocarbon.com>
2026-06-17 11:10:32 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "csl-back.azurewebsites.net",
+  baseURL: "https://csl-back.azurewebsites.net",
 });
 
 export const deleteResource = (endpoint, id) => api.delete(`${endpoint}/${id}`);
