083a10ee0c56f4fd5c6c54f394410da47191cbf4
STS-Engineer <administration.sts@avocarbon.com>
2026-06-17 14:00:16 +0100

    Fix unresolved merge conflict in api.js

--- src/api.js
+++ src/api.js
@@ -1,11 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-<<<<<<< HEAD
   baseURL: "https://csl-back.azurewebsites.net",
-=======
-  baseURL: "http://127.0.0.1:8000",
->>>>>>> 652f97d (update)
 });
 
 export const deleteResource = (endpoint, id) => api.delete(`${endpoint}/${id}`);
